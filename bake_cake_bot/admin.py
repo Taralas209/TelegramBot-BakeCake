9dc989776fdfae26bfb3ee78718698110e8d3ed1
Anton Ozerov <lamerork@gmail.com>
2023-07-28 19:04:32 +0300

    Добавил модель Cake

--- bake_cake_bot/admin.py
+++ bake_cake_bot/admin.py
@@ -1,9 +1,45 @@
 from django.contrib import admin
 
-from bake_cake_bot.models import Users
+from bake_cake_bot.models import *
 
 
 @admin.register(Users)
 class UsersAdmin(admin.ModelAdmin):
     list_display = ['id', 'telegram_id', 'username', 'name', 'is_admin']
     search_fields = ['telegram_id', 'username', 'phone']
+
+
+@admin.register(Order)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['number', 'username', 'price', 'init_date', 'delivery_date']
+
+
+@admin.register(Cake)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['layer', 'shape', 'topping', 'berries', 'decor']
+
+
+@admin.register(Layer)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+@admin.register(Shape)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+@admin.register(Topping)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+@admin.register(Berries)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+@admin.register(Decor)
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
--- bake_cake_bot/models.py
+++ bake_cake_bot/models.py
@@ -55,13 +55,14 @@ class Users(UUIDMixin, TimeStampedMixin):
         verbose_name = 'User'
         verbose_name_plural = 'Users'
 
+
 class Order(models.Model):
     number = models.AutoField(
         verbose_name='Номер заказа',
         primary_key=True,
         unique=True
     )
-    user = models.ForeignKey(
+    username = models.ForeignKey(
         Users, 
         on_delete=models.SET_NULL,
         null = True
@@ -87,6 +88,96 @@ class Order(models.Model):
         max_length=100,
         blank=True
     )
+    comment = models.TextField(
+        verbose_name='Комментарий',
+        max_length=1000,
+        blank=True
+    )
 
     def __str__(self):
         return f'Заказ номер {self.number}'
+
+
+class Layer(models.Model):
+    name = models.CharField(
+        verbose_name='Слой',
+        max_length=50
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Shape(models.Model):
+    name = models.CharField(
+        verbose_name='Форма',
+        max_length=50
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Topping(models.Model):
+    name = models.CharField(
+        verbose_name='Топпинг',
+        max_length=50
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Berries(models.Model):
+    name = models.CharField(
+        verbose_name='Ягоды',
+        max_length=50
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Decor(models.Model):
+    name = models.CharField(
+        verbose_name='Украшение',
+        max_length=50
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Cake(models.Model):
+    
+    layer = models.OneToOneField(Layer,  on_delete=models.SET_NULL, null = True)
+    shape = models.OneToOneField(Shape,  on_delete=models.SET_NULL, null = True)
+    topping = models.OneToOneField(Topping, on_delete=models.SET_NULL, null = True)
+    berries = models.OneToOneField(Berries, on_delete=models.SET_NULL, null = True)
+    decor = models.OneToOneField(Decor, on_delete=models.SET_NULL, null = True)
+    text = models.TextField(
+        verbose_name='Надпись на торте',
+        max_length=200,
+        blank=True
+    )
+    
