d8fa53bb474c9ac95fad82aeb0f7ac656fef468e
Anton Ozerov <lamerork@gmail.com>
2023-07-28 17:15:43 +0300

    Добавил модель Order

--- bake_cake_bot/models.py
+++ bake_cake_bot/models.py
@@ -54,3 +54,39 @@ class Users(UUIDMixin, TimeStampedMixin):
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+class Order(models.Model):
+    number = models.AutoField(
+        verbose_name='Номер заказа',
+        primary_key=True,
+        unique=True
+    )
+    user = models.ForeignKey(
+        Users, 
+        on_delete=models.SET_NULL,
+        null = True
+    )
+    price = models.FloatField(
+        verbose_name='Цена',
+        default=0.0
+    )
+    init_date = models.DateTimeField(
+        verbose_name='Дата создания заказа',
+        default=timezone.now()
+    )
+    delivery_date = models.DateTimeField(
+        verbose_name='Дата и время доставки',
+        default=None
+    )
+    address = models.TextField(
+        verbose_name='Адрес доставки',
+        max_length=500
+    )
+    promocode = models.CharField(
+        verbose_name='Промокод',
+        max_length=100,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'Заказ номер {self.number}'
